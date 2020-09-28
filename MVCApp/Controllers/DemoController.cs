de91626abf7acb84c85becd096a2d948eedc6ac9
Hien Luong <hyhien@gmail.com>
2020-09-29 06:55:10 +0700

    Demo Async

--- MVCApp/Controllers/DemoController.cs
+++ MVCApp/Controllers/DemoController.cs
@@ -1,9 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MVCApp.Controllers
 {
     public class DemoController : Controller
     {
+        public IActionResult DemoSync()
+        {
+            var demo = new Demo();
+            var dongHo = new Stopwatch();
+            dongHo.Start();
+            demo.FunctionA();
+            demo.FunctionB();
+            demo.FunctionC();
+            dongHo.Stop();
+
+            return Content($"Chạy hết {dongHo.ElapsedMilliseconds} ms");
+        }
+
+        [Route("/DemoAsync")]
+        public async Task<IActionResult> DemoAsync()
+        {
+            var demo = new Demo();
+            var dongHo = new Stopwatch();
+            dongHo.Start();
+            var a = demo.FunctionAAsync();
+            var b = demo.FunctionBAsync();
+            var c = demo.FunctionCAsync();
+            await a; await b; await c;
+            dongHo.Stop();
+
+            return Content($"Chạy hết {dongHo.ElapsedMilliseconds} ms");
+        }
+
+        //URL friendly
+        [Route("/dien-thoai/{tenDienThoai}")]
+        public string ABCDEF(string tenDienThoai)
+        {
+            return $"Điện thoại {tenDienThoai}";
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ MVCApp/Models/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class Demo
+    {
+        public string FunctionA()
+        {
+            Thread.Sleep(2000);
+            return "Hello";
+        }
+        public int FunctionB()
+        {
+            Thread.Sleep(5000);
+            return new Random().Next();
+        }
+        public void FunctionC()
+        {
+            Thread.Sleep(3000);
+        }
+        public async Task<string> FunctionAAsync()
+        {
+            await Task.Delay(2000);
+            return "Hello";
+        }
+        public async Task<int> FunctionBAsync()
+        {
+            await Task.Delay(5000);
+            return new Random().Next();
+        }
+        public async Task FunctionCAsync()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
