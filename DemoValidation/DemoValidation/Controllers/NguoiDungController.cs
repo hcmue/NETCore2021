f93116cb0ff724b47df1f4166ca91a7e06a37d76
Hien Luong <hyhien@gmail.com>
2020-10-23 07:57:18 +0700

    Basic jQueryValidtion

---
+++ DemoValidation/DemoValidation/Controllers/NguoiDungController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoValidation.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult XuLyDangKy()
+        {
+            return View();
+        }
+
+
+    }
+}
