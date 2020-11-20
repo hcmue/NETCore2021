044bcf7323c0f2f940de5d410125a5e75dcb8254
Hien Luong <hyhien@gmail.com>
2020-11-20 08:30:08 +0700

    Done Ajax

--- MyFinalProject/MyFinalProject/Startup.cs
+++ MyFinalProject/MyFinalProject/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyFinalProject.Entities;
 
 namespace MyFinalProject
 {
@@ -23,7 +25,8 @@ namespace MyFinalProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddDbContext<MyStore2020Context>(option => option.UseSqlServer(Configuration.GetConnectionString("MyStore2020")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +51,10 @@ namespace MyFinalProject
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
